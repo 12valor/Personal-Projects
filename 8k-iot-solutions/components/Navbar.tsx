874ab4b9bcb364e7adece6db5b036a3a858371e8
AG Evangelista <evangelista.agdiaz@gmail.com>
2026-03-26 03:01:54 +0800

    feat: added social link for facebook page

--- 8k-iot-solutions/components/Navbar.tsx
+++ 8k-iot-solutions/components/Navbar.tsx
@@ -126,7 +126,7 @@ export default function Navbar() {
 
           <div className="hidden md:flex flex-1 items-center justify-end gap-3">
             {/* Facebook */}
-            <a href="#" className="shrink-0 group flex items-center justify-start h-[50px] w-[50px] bg-white rounded-[10px] shadow-[0_4px_15px_rgba(0,0,0,0.06)] transition-all duration-300 hover:w-[190px] hover:rounded-[8px] overflow-hidden border border-gray-100">
+            <a href="https://www.facebook.com/profile.php?id=61586397291701" className="shrink-0 group flex items-center justify-start h-[50px] w-[50px] bg-white rounded-[10px] shadow-[0_4px_15px_rgba(0,0,0,0.06)] transition-all duration-300 hover:w-[190px] hover:rounded-[8px] overflow-hidden border border-gray-100">
               <div className="flex items-center justify-center min-w-[50px] h-[50px]">
                 <svg viewBox="0 0 320 512" className="h-6 w-6 fill-[#1877f2]" xmlns="http://www.w3.org/2000/svg"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>
               </div>
